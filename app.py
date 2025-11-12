8cf177a4b2c6cc8bb28ca6544398ba94e73cd891
ashrafyahya <ashrafsadiq97@gmail.com>
2025-11-12 14:20:34 +0100

    saving Embeddings Local

--- app.py
+++ app.py
@@ -1,29 +1,9 @@
 import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
-from llama_index.llms.groq import Groq
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding  
-from llama_index.core import Settings
+from llama_index.core import SimpleDirectoryReader
+from savingEmbeddingsLocal import load_or_create_index
 
-from dotenv import load_dotenv
-
-
-load_dotenv()
-groq_api_key = os.environ.get("groq_api_key")
-
-
-# Set the global settings & Configure all components in one place
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-Settings.chunk_size = 512
-Settings.chunk_overlap = 50
-Settings.llm = Groq(model="llama-3.1-8b-instant", api_key=groq_api_key)
-
-
-# Loading documents from a local directory
-documents = SimpleDirectoryReader("data").load_data()
-
-# processe the input documents to convert them into numerical vector representations
-# The VectorStoreIndex.from_documents() method typically creates an in-memory index
-index = VectorStoreIndex.from_documents(documents)
+# Load or create the index
+index = load_or_create_index()
 
 query_engine = index.as_query_engine()
 response = query_engine.query("What is RAG?")
